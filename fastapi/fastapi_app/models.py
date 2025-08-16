66b8a2cf5178f43325f45b93e7fb442dc60365f6
sawa <xj558y2d0290acc@gmail.com>
2025-08-16 14:18:47 +0900

    DB←→バックエンドの疎通を確認。エンドポイント「/locations/latlng」で、locationテーブルから緯度と経度を全件取得できる

--- fastapi/fastapi_app/models.py
+++ fastapi/fastapi_app/models.py
@@ -1,10 +1,15 @@
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Column, Integer, Float, DateTime
+from sqlalchemy.dialects.postgresql import INET
 from .database import Base
+from datetime import datetime
 
-class Post(Base):
-    __tablename__ = "post"
+class Location(Base):
+    __tablename__ = "locations"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(255), nullable=False)
-    username = Column(String(100), nullable=False)
-    text = Column(Text, nullable=False)
+    ip_address = Column(INET, nullable=False)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
+    accuracy = Column(Float)
+    scheduled_end_at = Column(DateTime(timezone=True))
+    created_at = Column(DateTime(timezone=True), default=datetime.utcnow)
